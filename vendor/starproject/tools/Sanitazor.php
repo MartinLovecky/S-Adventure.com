85b9d7fc5b63c77cd3b72ca09e84a4f7450c7fb4
Martin Lovecky <mlovecky@seznam.cz>
2020-08-19 19:16:09 +0200

    fixed const

--- vendor/starproject/tools/Sanitazor.php
+++ vendor/starproject/tools/Sanitazor.php
@@ -5,12 +5,12 @@ namespace starproject\tools;
 class Sanitazor{
 
     
-    const filter_default = FILTER_SANITIZE_FULL_SPECIAL_CHARS;
-    const filter_default_flags = 32;
+    const FILTER_DEFAULT = 522;
+    const FILTER_DEFAULT_FLAGS = 32;
 
     public function sanitaze_GET($string){
         $trimME = trim($string);
-        return \filter_input(INPUT_GET,$trimME,filter_default,filter_default_flags); 
+        return \filter_input(INPUT_GET,$trimME,self::FILTER_DEFAULT,self::FILTER_DEFAULT_FLAGS); 
     }
 
     public function sanitaze($string){
